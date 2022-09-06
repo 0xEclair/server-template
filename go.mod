aee0485e7aca54b2464675e9624238183166e63e
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-09-07 00:04:57 +0800

    :sparkles: feat: cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module server-template
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/joho/godotenv v1.4.0
--- server/route.go
+++ server/route.go
@@ -4,10 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"server-template/api"
+	"server-template/middleware"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middleware.Cors())
 
 	v1 := r.Group("/api/v1")
 	{
