774848b5751ba70c516f74a9630818c34515129d
0xEclair <38656355+0xEclair@users.noreply.github.com>
2023-08-23 15:20:44 +0800

    fix: path

--- third/bestinslot/sats.go
+++ third/bestinslot/sats.go
@@ -10,7 +10,7 @@ import (
 func AddressBySats(sats string) (string, error) {
 	resp, err := resty.New().R().
 		SetHeader("x-api-key", os.Getenv("bestinslot")).
-		Get(Base + "sats/forward_lookup?sats_name=" + sats)
+		Get(Base + "/sats/forward_lookup?sats_name=" + sats)
 	if err != nil {
 		return "", err
 	}
